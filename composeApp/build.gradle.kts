5934fb91f8e736624a9a5d79677c9652c0551bc6
raudonikis <raudonisernestas@gmail.com>
2026-02-19 15:26:39 +0200

    update dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.compose.ExperimentalComposeLibrary
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
-
 plugins {
     alias(libs.plugins.multiplatform)
     alias(libs.plugins.compose.compiler)
@@ -10,16 +7,7 @@ plugins {
 }
 
 kotlin {
-    androidTarget {
-        //https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-test.html
-        instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
-    }
-
-    js {
-        browser()
-        binaries.executable()
-    }
-
+    androidTarget()
     listOf(
         iosX64(),
         iosArm64(),
@@ -30,7 +18,6 @@ kotlin {
             isStatic = true
         }
     }
-
     sourceSets {
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -40,39 +27,22 @@ kotlin {
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
         }
-
-        commonTest.dependencies {
-            implementation(kotlin("test"))
-            @OptIn(ExperimentalComposeLibrary::class)
-            implementation(compose.uiTest)
-        }
-
         androidMain.dependencies {
             implementation(compose.uiTooling)
             implementation(libs.androidx.activityCompose)
         }
-
     }
 }
 
 android {
     namespace = "com.demo"
-    compileSdk = 35
-
+    compileSdk = 36
     defaultConfig {
-        minSdk = 21
-        targetSdk = 35
-
+        minSdk = 28
+        targetSdk = 36
         applicationId = "com.demo.androidApp"
         versionCode = 1
         versionName = "1.0.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-}
-
-//https://developer.android.com/develop/ui/compose/testing#setup
-dependencies {
-    androidTestImplementation(libs.androidx.uitest.junit4)
-    debugImplementation(libs.androidx.uitest.testManifest)
 }
